1885444c2c432b04ef61ca7dcf4c23961b31d277
hyeri0609 <hyeri_0609@protonmail.com>
2017-11-03 01:20:26 +0000

    Add array

--- functions.js
+++ functions.js
@@ -67,7 +67,78 @@ Items=[
 ];
 
 // Rooms
-
+var Rooms=new Array();
+Rooms = [
+	{
+		name: "Title",
+		title: "Text Adventures Are Fun",
+		description: "This is one of the best text adventures you will ever witness. Journey to the button \
+			and select if you dare to take such a marvelous journey!",
+		btnTxt: ["Let us begin"],
+		btnFnc: ['makeRoom(LivingRoom)']
+	},
+	{
+		name: "Closet",
+		title: "You are in a closet...",
+		description: "Well, this is a closet. not unlike every other closet you have seen.Pretty dark in here.",
+		btnTxt: ["go back to the living room"],
+		btnFnc: ['makeRoom(BedRoom)']
+	},
+	{
+		name: "BedRoom",
+		title: "What a messy bedroom",
+		description: "Woah this place is a mess! Clothes are scattered everywhere, there are dishes towards the edges of the clothes heap. Following a plug from the wall you see a laptop computer. A somewhat openable closet is in the corner. ",
+		btnTxt: ["turn on the computer", "enter the closet", "look around", "go back to the living room", 
+			"clean up the room"],
+		btnFnc: ['footTxt(\'Turning on the computer, it looks as if someone is making a silly text game.\')', 'makeRoom(Closet)', 
+			'footTxt(\'This place would make a normal person shiver.\')', 'makeRoom(LivingRoom)', 
+			'footTxt(\'HA HA Yea Right!\')']
+	},
+	{
+		name: "OutFront",
+		title: "Out in Front of the House",
+		description: "Ahh, good old outside. Seems to be raining a little bit. The grass is soaking wet, it looks like it has been raining for a long time. The front yard A big apple tree is fenced in the center of the yard. For some reason the front gate is locked preventing you from going anywhere else but back inside the house.",
+		btnTxt: ["go back inside", "grab an apple", "take a nap", "look around", "climb the tree",
+			"do a cartwheel", "check the mail", "shout at the rain"],
+		btnFnc: ['makeRoom(LivingRoom)', 'footTxt(\'Mmmm Preeettty good apple!\')', 
+			'footTxt(\'Yea right, like I am really going to take a nap in the rain.\')', 
+			'footTxt(\'Sure is rainy out here.\')', 'footTxt(\'Do you really think it iss a good idea to climb a tree in the rain?\')',
+			'footTxt(\'weeeeeeeeeeeee!\')', 'footTxt(\'Oh yea, that is right. Mail does not come on sundays.\')',
+			'footTxt(\'You Shout at the top of your lungs STOP RAINING YOU STUPID RAIN! you even shake your fist a little.\')']
+	},
+	{
+		name: "LivingRoom",
+		title: "The Living Room",
+		description: "You are in the main living room of the house, there are normal living room things here, a tv, a couch and chair. A door leads to the bedroom, and another door leads out to the front yard. a dining room stretches out across the hallway.",
+		btnTxt: ["watch tv", "look out the window", "take a nap", "look around", "sneeze", "go out the front door", 
+			"go into the bedroom", "enter the dining room", "You lose"],
+		btnFnc: ['footTxt(\'You spend some minutes clicking through channels, as usual nothings on.\')', 'footTxt(\'You look out the window, seems like its raining outside.\')', 
+			'footTxt(\'You snooze off for a bit, no cool dreams or anthing though.\')', 'footTxt(\'Looks like this room could use some cleaning, I am not gonna be the one to do it though.\')', 
+			'footTxt(\'Achoooooo!\')', 'makeRoom(OutFront)', 'makeRoom(BedRoom)', 
+			'makeRoom(DiningRoom)', 'makeRoom(DefeatRoom)']
+	},
+	{
+		name: "VictoryRoom",
+		title: "You have won the game",
+		description: "This is the winners room",
+		btnTxt: ["Finish the Game"],
+		btnFnc: ['']
+	},
+	{
+		name: "DefeatRoom",
+		title: "You have lost the game",
+		description: "This is the losers room",
+		btnTxt: ["Finish the Game"],
+		btnFnc: ['']
+	},
+	{
+		name: "Inventory",
+		title: "Inventory",
+		description: "Inventory",
+		btnTxt: ["Finish the Game"],
+		btnFnc: ['']
+	},
+];
 // Title scene
 var Title= {
 	name: "Title",
